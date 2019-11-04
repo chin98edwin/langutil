01f9e208615e34150f1d3994e32935fae2ec24be
chin98edwin <edwin_chin98@hotmail.com>
2019-11-04 15:26:53 +0800

    Updates

---
+++ precache-manifest.96faca854358976e16785929aa5e91a1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc6ca3212974af7948e107eb5d23209",
+    "url": "/langutil/index.html"
+  },
+  {
+    "revision": "9887a6bd74669e508914",
+    "url": "/langutil/static/css/0.59fee26f.chunk.css"
+  },
+  {
+    "revision": "6936bda8c52d3b3b83c4",
+    "url": "/langutil/static/css/4.d8257110.chunk.css"
+  },
+  {
+    "revision": "ae651bc6e8f0acb02aee",
+    "url": "/langutil/static/css/5.0ebd6c73.chunk.css"
+  },
+  {
+    "revision": "8d15de4fe2bf0bfe8521",
+    "url": "/langutil/static/css/6.87544b52.chunk.css"
+  },
+  {
+    "revision": "8ee0a3d478faed5a3d0e",
+    "url": "/langutil/static/css/main.295b288e.chunk.css"
+  },
+  {
+    "revision": "9887a6bd74669e508914",
+    "url": "/langutil/static/js/0.945fe96d.chunk.js"
+  },
+  {
+    "revision": "72d1011a27a7b1821625",
+    "url": "/langutil/static/js/10.0082e373.chunk.js"
+  },
+  {
+    "revision": "613cce1be2ddff8c29f4",
+    "url": "/langutil/static/js/3.53efa620.chunk.js"
+  },
+  {
+    "revision": "6936bda8c52d3b3b83c4",
+    "url": "/langutil/static/js/4.e4e7f91c.chunk.js"
+  },
+  {
+    "revision": "ae651bc6e8f0acb02aee",
+    "url": "/langutil/static/js/5.180259f9.chunk.js"
+  },
+  {
+    "revision": "8d15de4fe2bf0bfe8521",
+    "url": "/langutil/static/js/6.9e677100.chunk.js"
+  },
+  {
+    "revision": "f0aa10356110154b3429",
+    "url": "/langutil/static/js/7.40369aef.chunk.js"
+  },
+  {
+    "revision": "5203e7e1e073a23e6aee",
+    "url": "/langutil/static/js/8.ba6f99f1.chunk.js"
+  },
+  {
+    "revision": "0b16744ca98a2aa080b6",
+    "url": "/langutil/static/js/9.21dda1ba.chunk.js"
+  },
+  {
+    "revision": "8ee0a3d478faed5a3d0e",
+    "url": "/langutil/static/js/main.1a1c22b3.chunk.js"
+  },
+  {
+    "revision": "2b6955fe36cd0deb40d8",
+    "url": "/langutil/static/js/runtime-main.15d5cc4c.js"
+  },
+  {
+    "revision": "57b5d819f476cd370d6bddd826da6962",
+    "url": "/langutil/static/media/github-icon.57b5d819.svg"
+  },
+  {
+    "revision": "6370a5d28cf32545c7fd8b6e34c72288",
+    "url": "/langutil/static/media/langutil-icon.6370a5d2.svg"
+  }
+]);
